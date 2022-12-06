b38104c459327aa56d87ebcac5667ac11be31ab0
Gwanghee Jo <92922343+Jojorabbit21@users.noreply.github.com>
2022-12-06 18:01:10 +0900

    daily
    haahahahaha

--- src/app/bref_schedule.py
+++ src/app/bref_schedule.py
@@ -10,8 +10,7 @@ sys.path.append('.')
 import datetime
 from time import sleep
 from bs4 import BeautifulSoup
-
-from src.app.findplayer import get_player_info
+from unidecode import unidecode
 
 base_header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
@@ -56,33 +55,34 @@ full_to_abbr = {
     'Washington Wizards': 'WAS'
 }
 
-def calculate_minutes(data, playerinfo):
-    team_id = playerinfo.loc[:, "TEAM_ID"].item()
-    player_id = playerinfo.loc[:, "PERSON_ID"].item()
-    player_name = playerinfo.loc[:, "DISPLAY_FIRST_LAST"].item()
-    
-    mp = data['MinutesPlayed'].to_numpy(dtype=np.float64)
-    io = data['Status'].to_numpy(dtype=np.int32)    
-    
-    arr_mp = np.ndarray(shape=(1,))
-    arr_io = np.ndarray(shape=(1,))
+def load_player_list():
+    return pd.read_csv('src/data/static/players_bref.csv')
 
+def get_player_info(playername:str):
+    player_list = load_player_list()
+    playername = unidecode(playername)
+    search = player_list[player_list['Player'] == playername]
+    # if len(search) > 1:
+    #     raise ValueError('Too many results.')
+    return search
+
+def calculate_minutes(data, playerinfo, match_info:list):
+    player_name = playerinfo.loc[:, "Player"].item()
+    
+    date = match_info[0]
+    team = match_info[1]
+    against = match_info[2]
+    
+    arr_mp = data['MinutesPlayed'].to_numpy(dtype=np.float64)
+    arr_io = data['Status'].to_numpy(dtype=np.int32)    
+    
     # Integrate consecutive playing times
-    l = len(mp)
-    for i in range(0, l, 2):
-        if i < l-1:
-            if io[i] == io[i+1]:
-                arr_mp = np.append(arr_mp, np.sum(mp[i:i+2]))
-                arr_io = np.append(arr_io, [io[i]])
-            else:
-                arr_mp = np.append(arr_mp, [mp[i:i+2]])
-                arr_io = np.append(arr_io, [io[i:i+2]])
-        elif i == l-1:
-            arr_mp = np.append(arr_mp, [mp[i]])
-            arr_io = np.append(arr_io, [io[i]])
-            
-    arr_mp = np.delete(arr_mp, 0)
-    arr_io = np.delete(arr_io, 0)   
+    for _, i in enumerate(arr_mp):
+        if _ < len(arr_mp) - 1:
+            if arr_io[_] == arr_io[_+1]:
+                arr_mp[_] = arr_mp[_] + arr_mp[_ + 1]
+                arr_mp = np.delete(arr_mp, _+1)
+                arr_io = np.delete(arr_io, _+1)
     
     # Convert to stacked-timestamp
     for _, i in enumerate(arr_mp):
@@ -103,32 +103,79 @@ def calculate_minutes(data, playerinfo):
         need = 2880 - arr_mp[-1]
         arr_mp[-1] = arr_mp[-1] + need
         
-    print(arr_mp)
+    # time = []
+    # for i in arr_mp:
+    #     a = datetime.timedelta(seconds=i)
+    #     time.append(str(a))
+    # print(time)
     
-    min_arr = np.zeros(shape=(48,))
+    min_arr = np.zeros(shape=(49,))
+    min_count = 0
     for _, i in enumerate(arr_mp):
-        if arr_io[_] == 1:
+        if _ == 0:
             duration = datetime.timedelta(seconds=i)
             duration_m = (duration.seconds // 60) % 60
             duration_s = duration.seconds - (duration_m * 60)
-            if _ == 0:
-                start_m = 0
+            # print(f'({arr_io[_]}) 0:00:00 ~ {str(duration)} / {str(duration)} play/rest')
+            if arr_io[_] == 1:
+                for tick in range(0, duration_m):
+                    min_arr[tick] = 60
+            if duration_s > 0:
+                min_arr[duration_m] = duration_s
+                min_count = duration_m
             else:
-                start = datetime.timedelta(seconds=arr_mp[_-1])
-                start_m = (start.seconds // 60) % 60
-                start_s = start.seconds - (start_m * 60)
-                if start_s > 0:
-                    start_m += 1
-            for tick in range(duration_m):
-                if start_m + tick < 48:
-                    min_arr[start_m + tick] = 1
-    print(min_arr)
+                min_count = duration_m - 1
+        else:
+            last_pos = min_count
+            duration = datetime.timedelta(seconds= i - arr_mp[_-1])
+            duration_m = (duration.seconds // 60) % 60
+            duration_s = duration.seconds - (duration_m * 60)
+            start = datetime.timedelta(seconds=arr_mp[_-1])
+            if min_arr[last_pos] > 0: # when float exist
+                x = 60 - min_arr[last_pos] # need to fill up rest of minute
+                if x > duration_s:
+                    duration_s = 60 - (x - duration_s)
+                    duration_m -= 1
+                elif x == duration_s:
+                    duration_s = 0 
+                elif x < duration_s:
+                    duration_s = duration_s - x
+                if arr_io[_] == 1:
+                    min_arr[last_pos] = 60 - min_arr[last_pos]
+            last_pos += 1
+            for tick in range(last_pos, (last_pos + duration_m)):
+                if arr_io[_] == 1:
+                    min_arr[tick] = 60
+                # else:
+                #     min_arr[tick] = 0
+            # print(f'({arr_io[_]}) {str(start)} ~ {str(duration + start)} / {str(duration)} play/rest')
+            if _ < len(arr_mp) - 1:
+                if duration_s > 0:
+                    min_arr[last_pos + duration_m] = duration_s
+                    min_count += (duration_m + 1)
+                else:
+                    min_count += duration_m
+        # print(min_arr)
+    min_arr = np.delete(min_arr, -1)
     
+    def fun(e):
+        return e / 60
     
+    vectorize = np.vectorize(fun)
+    min_arr = vectorize(min_arr)
     
+    m_info = [player_name, date, team, against]
+    m_df = pd.DataFrame([m_info], columns=['PlayerName', 'Date', 'Team', 'Opp'])
+    df = pd.DataFrame([min_arr])
+    df = pd.concat([m_df, df], axis=1)
     
-    
-
+    parent = f'src/data/teamdashplayers/'
+    if os.path.isdir(parent + str(player_name)):
+        df.to_csv(parent + str(player_name) + "/" + f'{player_name}.csv', mode='a', header=None)
+    else:
+        os.mkdir(parent + str(player_name))
+        df.to_csv(parent + str(player_name) +  "/" +  f'{player_name}.csv', mode='w')
+            
 def bref_scrape_schedule(seasons:list=[2022]):
     base_url = 'https://www.basketball-reference.com/leagues/NBA_{}_games-{}.html' # ex) 2023 => 2022-23
     boxscore_url = []
@@ -157,11 +204,13 @@ def bref_scrape_schedule(seasons:list=[2022]):
             else:
                 print('no datas fetched')
                 print(response.status_code)
-
+    fetched = pd.DataFrame(0, index=np.zeros(shape=(len(boxscore_url), )), columns=['fetched'])
     df = pd.DataFrame(data=boxscore_url, columns=['boxscore_url'])
+    df = pd.concat([df, fetched], axis=1)
+    season = f'{int(season)-1}{season[-2:]}'
     df.to_csv(f'src/data/schedules/bref_{season}.csv')
 
-def bref_scrape_chart(url:str):
+def bref_scrape_chart(url:str, season):
     response = requests.get(url, headers=base_header)
     html = response.text
     soup = BeautifulSoup(html, 'lxml')
@@ -173,13 +222,8 @@ def bref_scrape_chart(url:str):
     home_team = full_to_abbr[soup.select_one('#content > div.scorebox > div:nth-child(2) > div:nth-child(1) > strong > a').text]
     filename = f'{date}_{away_team}@{home_team}'
     
-    if not os.path.isdir(filename):
-        os.mkdir(filename)
-        os.mkdir(filename + "/" + away_team)
-        os.mkdir(filename + "/" + home_team)
-
     table = soup.select_one('#content > div.plusminus > div')
-    table_width = re.findall("\:([0-9]*)\w", table['style'])[0]
+    table_width = re.findall("\W([0-9]*)\w", table['style'])[0]
 
     away_table = soup.select_one('#content > div.plusminus > div > div:nth-child(1)')
     home_table = soup.select_one('#content > div.plusminus > div > div:nth-child(2)')
@@ -189,40 +233,51 @@ def bref_scrape_chart(url:str):
     home_player = home_table.select('div.player')
     home_bar = home_table.select('div.player-plusminus')
 
-    # away_length = len(away_player)
-    # print('Creating Away Players timetable...')
-    # for idx in range(0, away_length):
-    #     player_name = re.findall("(.*)\s\(", away_player[idx].text)[0]
-    #     player_info = get_player_info(player_name)
-    #     bars = away_bar[idx].select('div')
-    #     actual_width = int(table_width) - 1
-    #     status = []; minute = []; margin = []
-    #     for rows in bars:
-    #         time_width = int(re.findall("\:([0-9]*)\w",rows['style'])[0]) + 1
-    #         actual_minute = round(time_width / actual_width,2)
-    #         actual_minute = round(48 * actual_minute,2)
-    #         try:
-    #             if rows['class'][0] == 'minus' or rows['class'][0] == 'plus':
-    #                 status.append(1)
-    #                 margin.append(int(rows.text))
-    #         except:
-    #             status.append(0)
-    #             margin.append(0)
-    #         finally:
-    #             minute.append(actual_minute)
-    #     df = pd.DataFrame(data=[
-    #         minute, 
-    #         # margin, 
-    #         status
-    #         ])
-    #     df = df.T
-    #     df.columns = [
-    #         'MinutesPlayed', 
-    #         # 'ScoreMargin', 
-    #         'Status'
-    #         ]
-    #     df.to_csv(f'{filename}/{away_team}/{player_name}.csv')
-    #     calculate_minutes(df, player_info)
+    away_length = len(away_player)
+    print('Creating Away Players timetable...')
+    for idx in range(0, away_length):
+        player_name = re.findall("(.*)\s\(", away_player[idx].text)[0]
+        print(player_name)
+        player_info = get_player_info(player_name)
+        bars = away_bar[idx].select('div')
+        actual_width = int(table_width) - 1
+        status = np.zeros(shape=(len(bars),), dtype=np.int16)
+        minute = np.zeros(shape=(len(bars),), dtype=np.float64)
+        margin = np.zeros(shape=(len(bars),), dtype=np.int16)
+        for i, rows in enumerate(bars):
+            if rows['style'][0] != 'width:-1px;':
+                time_width = int(re.findall("\W([0-9]*)\w",rows['style'])[0]) + 1
+                actual_minute = time_width / actual_width
+                actual_minute = round(2880 * actual_minute, 1)
+                try:
+                    if rows['class'][0] == 'minus' or rows['class'][0] == 'plus' or rows['class'][0] == 'even':
+                        status[i] = 1
+                        margin[i] = int(rows.text)
+                except:
+                        status[i] = 0
+                        margin[i] = 0
+                finally:
+                    minute[i] = actual_minute
+        df = pd.DataFrame(data=[
+            minute, 
+            # margin, 
+            status
+            ])
+        df = df.T
+        df.columns = [
+            'MinutesPlayed', 
+            # 'ScoreMargin', 
+            'Status'
+            ]        
+        if not os.path.isdir(f'src/data/boxscores/{season}'):
+            os.mkdir(f'src/data/boxscores/{season}')
+        if not os.path.isdir(f'src/data/boxscores/{season}/{filename}'):
+            os.mkdir(f'src/data/boxscores/{season}/{filename}')
+        if not os.path.isdir(f'src/data/boxscores/{season}/{filename}/{away_team}'):
+            os.mkdir(f'src/data/boxscores/{season}/{filename}/{away_team}')
+        df.to_csv(f'src/data/boxscores/{season}/{filename}/{away_team}/{player_name}.csv')
+        match_info = [date, away_team, home_team]
+        calculate_minutes(df, player_info, match_info)
         
     home_length = len(home_player)
     print('Creating Home Players timetable...')
@@ -236,18 +291,19 @@ def bref_scrape_chart(url:str):
         minute = np.zeros(shape=(len(bars),), dtype=np.float64)
         margin = np.zeros(shape=(len(bars),), dtype=np.int16)
         for i, rows in enumerate(bars):
-            time_width = int(re.findall("\:([0-9]*)\w",rows['style'])[0]) + 1
-            actual_minute = time_width / actual_width
-            actual_minute = round(2880 * actual_minute, 1)
-            try:
-                if rows['class'][0] == 'minus' or rows['class'][0] == 'plus':
-                    status[i] = 1
-                    margin[i] = int(rows.text)
-            except:
-                    status[i] = 0
-                    margin[i] = 0
-            finally:
-                minute[i] = actual_minute
+            if rows['style'][0] != 'width:-1px;':
+                time_width = int(re.findall("\W([0-9]*)\w",rows['style'])[0]) + 1
+                actual_minute = time_width / actual_width
+                actual_minute = round(2880 * actual_minute, 1)
+                try:
+                    if rows['class'][0] == 'minus' or rows['class'][0] == 'plus':
+                        status[i] = 1
+                        margin[i] = int(rows.text)
+                except:
+                        status[i] = 0
+                        margin[i] = 0
+                finally:
+                    minute[i] = actual_minute
         df = pd.DataFrame(data=[
             minute, 
             # margin, 
@@ -259,8 +315,41 @@ def bref_scrape_chart(url:str):
             # 'ScoreMargin', 
             'Status'
             ]
-        df.to_csv(f'{filename}/{home_team}/{player_name}.csv')
-        calculate_minutes(df, player_info)
+    
+        if not os.path.isdir(f'src/data/boxscores/{season}'):
+            os.mkdir(f'src/data/boxscores/{season}')
+        if not os.path.isdir(f'src/data/boxscores/{season}/{filename}'):
+            os.mkdir(f'src/data/boxscores/{season}/{filename}')
+        if not os.path.isdir(f'src/data/boxscores/{season}/{filename}/{home_team}'):
+            os.mkdir(f'src/data/boxscores/{season}/{filename}/{home_team}')
+        df.to_csv(f'src/data/boxscores/{season}/{filename}/{home_team}/{player_name}.csv')
+        match_info = [date, home_team, away_team]
+        calculate_minutes(df, player_info, match_info)
 
 
-bref_scrape_chart('https://www.basketball-reference.com/boxscores/plus-minus/202211280BOS.html')
+
+# For test
+# bref_scrape_chart('https://www.basketball-reference.com/boxscores/plus-minus/202110200DET.html', '202223')
+
+season = '202122'
+bref_base = 'https://www.basketball-reference.com/boxscores/plus-minus/'
+filepath = 'src/data/schedules/bref.com/'
+filelist = os.listdir(filepath)
+for file in filelist:
+    df = pd.read_csv(filepath + file)
+    for row in df.itertuples():
+        if row.fetched == 0:
+            boxscore_url = str(row.boxscore_url).split("/")[-1]
+            url = bref_base + boxscore_url
+            print(url)
+            bref_scrape_chart(url=url, season=season)
+            df.iloc[row.Index, -1] = 1
+            df.to_csv(filepath + file, index=False)
+            sleep(1.5)
+
+# ---------* Sanitize BBref player list names  
+# player_list = pd.read_csv('src/data/static/players_bref.csv')
+# for row in player_list.itertuples():
+#     name = unidecode(row.Player)
+#     player_list.iloc[row.Index, 0] = name
+# player_list.to_csv('src/data/static/players_bref.csv')
